e1d3b5508550a498bfaec1ac64f6b2e0114071a8
KeThien Nguyen <giakethien@gmail.com>
2017-11-01 15:54:49 +0100

    update controllers and views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,53 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category )
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
+
+  # create_table "restaurants", force: :cascade do |t|
+  #   t.string   "name"
+  #   t.string   "address"
+  #   t.string   "phone_number"
+  #   t.string   "category"
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,11 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian)}
+
 end
+
+  # validates :stars, inclusion: { in: [1, 2, 3], allow_nil: false }
+  # validates :name, uniqueness: true, presence: true
+  # validates :address, presence: true
